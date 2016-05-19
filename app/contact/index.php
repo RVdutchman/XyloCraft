2e2a7b5f4cd7d39ba5a8c55f9552ba680c819aea
RVdutchman <rvoetman1@outlook.com>
2016-05-19 13:24:42 +0200

    Updated Contact page

--- app/contact/index.php
+++ app/contact/index.php
@@ -32,32 +32,47 @@
          <img class="img-responsive headerImg" src="../img/XyloCraft_Banner.png" alt="bannerImg">
       </div>
     </div>
-<!-- Widget -->
-<a class="twitter-timeline" href="https://twitter.com/PotterFreakRoy/lists/xylocraft" data-widget-id="733223677978943488">Tweets van https://twitter.com/PotterFreakRoy/lists/xylocraft</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      <br>
+      <div class="row">
+          <div class="col-md-6">
+            <center>
+              <blockquote class="twitter-video" data-lang="nl"><p lang="en" dir="ltr">For our new followers, watch this video of our server trailer ;-). Ip <a href="https://t.co/LlViQNbL8n">https://t.co/LlViQNbL8n</a> or 212.129.11.28:11032 <a href="https://t.co/y4NYH4NV4d">pic.twitter.com/y4NYH4NV4d</a></p>&mdash; XyloCraftSupport (@XyloSupport) <a href="https://twitter.com/XyloSupport/status/728763752431165441">7 mei 2016</a></blockquote>
+              <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+              <hr>
+              <center>
+              <!-- tweet button -->
+                  <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.rvdutchman.xyz/XyloCraft" data-text="Come and join the XyloCraft community" data-via="XyloSupport" data-size="large">Tweet</a>
+                  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+              <!-- follow button -->
+                  <a href="https://twitter.com/XyloSupport" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @XyloSupport</a>
+                  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+              <!-- tweet naar -->
+                  <a href="https://twitter.com/intent/tweet?screen_name=XyloSupport" class="twitter-mention-button" data-size="large" data-related="PotterFreakRoy">Tweet to @XyloSupport</a>
+                  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script\', 'twitter-wjs');</script>
+            </center>
+            <hr>
+            <a class="twitter-timeline" href="https://twitter.com/hashtag/XyloCraft" data-widget-id="733248905329188864">Tweets over #XyloCraft</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          </center>
+          </div>
+          <div class="col-md-6">
+            <center>
+              <!-- Widget -->
+              <a class="twitter-timeline" href="https://twitter.com/PotterFreakRoy/lists/xylocraft" data-widget-id="733223677978943488">Tweets van https://twitter.com/PotterFreakRoy/lists/xylocraft</a>
+              <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </center>
+          </div>
+      </div>
+      <footer id="footer"></footer>
+     </div>
+   </div>
+    <script type="text/javascript" id="js">
+    $( document ).ready(function() {
+          MakeFooter("contact");
+    });
+    elem = document.getElementById('js');
+    elem.parentNode.removeChild(elem);
+    </script>
 
-<br>
-<!-- tweet button -->
-    <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.rvdutchman.xyz/XyloCraft" data-text="Come and you the XyloCraft community" data-via="XyloSupport" data-size="large">Tweet</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-<!-- follow button -->
-    <a href="https://twitter.com/XyloSupport" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @XyloSupport</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-<!-- tweet naar -->
-    <a href="https://twitter.com/intent/tweet?screen_name=XyloSupport" class="twitter-mention-button" data-size="large" data-related="PotterFreakRoy">Tweet to @XyloSupport</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
-    <footer id="footer"></footer>
-  </div>
-  </div>
-  <script type="text/javascript" id="js">
-  $( document ).ready(function() {
-        MakeFooter("contact");
-  });
-  elem = document.getElementById('js');
-  elem.parentNode.removeChild(elem);
-  </script>
-
-</body>
-
+  </body>
 </html>
